c1ecfd1fb368d5109d7563956dcc5fa7ab9f5780
Nguyễn Ngọc Thái Nguyên <19521917@users.noreply.github.com>
2022-06-03 17:50:54 +0700

    test deploy

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,6 @@ export const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_EXPIRE + 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
